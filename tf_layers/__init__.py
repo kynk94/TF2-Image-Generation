f53a859ae2e0a9c6bfe9043ece1c55c63ea2b733
kynk94 <kynk94@naver.com>
2020-12-16 20:40:17 +0900

    add DecompTransConvBlock

--- tf_layers/__init__.py
+++ tf_layers/__init__.py
@@ -8,16 +8,17 @@ from tensorflow.keras.layers import Activation, ReLU, LeakyReLU
 from tensorflow.keras.layers import BatchNormalization, Dropout
 from tensorflow_addons.layers import Maxout
 from .conv import Conv1D, Conv2D, Conv3D
-from .conv import DownConv1D, DownConv2D, DownConv3D
-from .conv import SubPixelConv2D
 from .conv import TransposeConv1D, TransposeConv2D, TransposeConv3D
 from .conv import DecompTransConv2D, DecompTransConv3D
+from .conv import DownConv1D, DownConv2D, DownConv3D
 from .conv import UpConv1D, UpConv2D, UpConv3D
+from .conv import SubPixelConv2D
 from .conv_blocks import Conv1DBlock, Conv2DBlock, Conv3DBlock
-from .conv_blocks import DownConv1DBlock, DownConv2DBlock, DownConv3DBlock
-from .conv_blocks import SubPixelConv2DBlock
 from .conv_blocks import TransConv1DBlock, TransConv2DBlock, TransConv3DBlock
+from .conv_blocks import DecompTransConv2DBlock, DecompTransConv3DBlock
+from .conv_blocks import DownConv1DBlock, DownConv2DBlock, DownConv3DBlock
 from .conv_blocks import UpConv1DBlock, UpConv2DBlock, UpConv3DBlock
+from .conv_blocks import SubPixelConv2DBlock
 from .dense import Dense, DenseBlock
 from .embedding import Embedding
 from .noise import GaussianNoise, UniformNoise
