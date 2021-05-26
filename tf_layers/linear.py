3a61d80605cf4f933695673837964d2fa649adac
kynk94 <kynk94@naver.com>
2021-05-26 17:55:33 +0900

    update Linear layer

--- tf_layers/linear.py
+++ tf_layers/linear.py
@@ -1,6 +1,5 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow.python.keras.layers.ops import core as core_ops
 from .normalizations import Normalization
 from .utils import get_activation_layer, get_noise_layer
 from .utils import get_layer_config
@@ -67,12 +66,45 @@ class Linear(tf.keras.layers.Dense):
             kernel = self.kernel * self.runtime_coef
         else:
             kernel = self.kernel
-        outputs = core_ops.dense(
-            inputs=inputs,
-            kernel=kernel,
-            bias=None,
-            activation=None,
-            dtype=self._compute_dtype_object)
+
+        if inputs.dtype.base_dtype != self._compute_dtype_object.base_dtype:
+            inputs = tf.cast(inputs, dtype=self._compute_dtype_object)
+
+        rank = inputs.shape.rank
+        if rank == 2 or rank is None:
+        # We use embedding_lookup_sparse as a more efficient matmul operation for
+        # large sparse input tensors. The op will result in a sparse gradient, as
+        # opposed to sparse_ops.sparse_tensor_dense_matmul which results in dense
+        # gradients. This can lead to sigfinicant speedups, see b/171762937.
+            if isinstance(inputs, tf.SparseTensor):
+                # We need to fill empty rows, as the op assumes at least one id per row.
+                inputs, _ = tf.sparse.fill_empty_rows(inputs, 0)
+                # We need to do some munging of our input to use the embedding lookup as
+                # a matrix multiply. We split our input matrix into separate ids and
+                # weights tensors. The values of the ids tensor should be the column
+                # indices of our input matrix and the values of the weights tensor
+                # can continue to the actual matrix weights.
+                # The column arrangement of ids and weights
+                # will be summed over and does not matter. See the documentation for
+                # sparse_ops.sparse_tensor_dense_matmul a more detailed explanation
+                # of the inputs to both ops.
+                ids = tf.SparseTensor(
+                    indices=inputs.indices,
+                    values=inputs.indices[:, 1],
+                    dense_shape=inputs.dense_shape)
+                weights = inputs
+                outputs = tf.nn.embedding_lookup_sparse(
+                    kernel, ids, weights, combiner='sum')
+            else:
+                outputs = tf.raw_ops.MatMul(a=inputs, b=kernel)
+        # Broadcast kernel to inputs.
+        else:
+            outputs = tf.tensordot(inputs, kernel, [[rank - 1], [0]])
+        # Reshape the output back to the original ndim of the input.
+            if not tf.executing_eagerly():
+                shape = inputs.shape.as_list()
+                output_shape = shape[:-1] + [kernel.shape[-1]]
+                outputs.set_shape(output_shape)
 
         if self.noise:
             outputs = self.noise(outputs)
